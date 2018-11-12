3936cd619007bea90d95dc4b50142c66d0d20aed
JoeSvr95 <joesaverio@hotmail.com>
2018-11-12 02:55:19 -0500

    Adding crosshair, fixing bugs

---
+++ Assets/Scripts/MouseController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class MouseController : MonoBehaviour {
+
+	private Rect screenRect;
+
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+		Rect screenRect = new Rect(0,0, Screen.width, Screen.height);
+	}
+
+	/*void Update(){
+		if (!screenRect.Contains(Input.mousePosition)) {
+			GameObject.FindGameObjectWithTag("Player").GetComponent<FirstPersonController>().enabled = false;
+		} else {
+			GameObject.FindGameObjectWithTag("Player").GetComponent<FirstPersonController>().enabled = true;
+		}
+	}*/
+	
+	void OnApplicationFocus(bool ApplicationIsBack){
+		if (ApplicationIsBack == true){
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+	}
+}
