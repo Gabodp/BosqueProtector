1113d68c31b84595196efcc02c4cb24b2b41c781
JoeSvr95 <joesaverio@hotmail.com>
2018-12-09 22:38:10 -0500

    Incorporación del MouseController

---
+++ Assets/Scripts/MouseController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class MouseController : MonoBehaviour {
+
+	private Rect screenRect;
+
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+		screenRect = new Rect(0,0, Screen.width, Screen.height);
+	}
+
+	void Update(){
+		if (!GameManager.instance.paused){
+			if (screenRect.Contains(Input.mousePosition)) {
+				GameObject.FindGameObjectWithTag("Player").GetComponent<FirstPersonController>().enabled = true;
+			} else {
+				GameObject.FindGameObjectWithTag("Player").GetComponent<FirstPersonController>().enabled = false;
+			}
+		}
+	}
+	
+	void OnApplicationFocus(bool ApplicationIsBack){
+		if (ApplicationIsBack == true){
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+	}
+}
